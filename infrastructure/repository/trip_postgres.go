4d76ca18283d3618e4df8d91d9ba98b0068b7b53
hata0 <haataa0100@gmail.com>
2025-07-13 23:23:25 +0900

    feat: TripPostgresRepositoryを作成

---
+++ infrastructure/repository/trip_postgres.go
@@ -0,0 +1,119 @@
+package repository
+
+import (
+	"context"
+	"travel-api/domain"
+	"travel-api/infrastructure/database"
+
+	"github.com/jackc/pgx/v5/pgtype"
+)
+
+type TripPostgresRepository struct {
+	queries *database.Queries
+}
+
+func NewTripPostgresRepository(queries *database.Queries) domain.TripRepository {
+	return TripPostgresRepository{
+		queries: queries,
+	}
+}
+
+func (r TripPostgresRepository) FindByID(ctx context.Context, id domain.TripID) (domain.Trip, error) {
+	var validatedId pgtype.UUID
+	if err := validatedId.Scan(id); err != nil {
+		return domain.Trip{}, err
+	}
+
+	record, err := r.queries.GetTrip(ctx, validatedId)
+	if err != nil {
+		return domain.Trip{}, err
+	}
+
+	return r.mapToTrip(record), nil
+}
+
+func (r TripPostgresRepository) FindMany(ctx context.Context) ([]domain.Trip, error) {
+	records, err := r.queries.ListTrips(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	trips := make([]domain.Trip, len(records))
+	for i, record := range records {
+		trips[i] = r.mapToTrip(record)
+	}
+
+	return trips, nil
+}
+
+func (r TripPostgresRepository) mapToTrip(record database.Trip) domain.Trip {
+	return domain.NewTrip(
+		domain.TripID(record.ID.String()),
+		record.Name,
+		record.CreatedAt.Time,
+		record.UpdatedAt.Time,
+	)
+}
+
+func (r TripPostgresRepository) Create(ctx context.Context, trip domain.Trip) error {
+	var validatedId pgtype.UUID
+	if err := validatedId.Scan(trip.ID); err != nil {
+		return err
+	}
+
+	var validatedCreatedAt pgtype.Timestamptz
+	if err := validatedCreatedAt.Scan(trip.CreatedAt); err != nil {
+		return err
+	}
+
+	var validatedUpdatedAt pgtype.Timestamptz
+	if err := validatedUpdatedAt.Scan(trip.UpdatedAt); err != nil {
+		return err
+	}
+
+	if err := r.queries.CreateTrip(ctx, database.CreateTripParams{
+		ID:        validatedId,
+		Name:      trip.Name,
+		CreatedAt: validatedCreatedAt,
+		UpdatedAt: validatedUpdatedAt,
+	}); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r TripPostgresRepository) Update(ctx context.Context, trip domain.Trip) error {
+	var validatedId pgtype.UUID
+	if err := validatedId.Scan(trip.ID); err != nil {
+		return err
+	}
+
+	var validatedUpdatedAt pgtype.Timestamptz
+	if err := validatedUpdatedAt.Scan(trip.UpdatedAt); err != nil {
+		return err
+	}
+
+	if err := r.queries.UpdateTrip(ctx, database.UpdateTripParams{
+		ID:        validatedId,
+		Name:      trip.Name,
+		UpdatedAt: validatedUpdatedAt,
+	}); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r TripPostgresRepository) Delete(ctx context.Context, trip domain.Trip) error {
+	var validatedId pgtype.UUID
+	if err := validatedId.Scan(trip.ID); err != nil {
+		return err
+	}
+
+	if err := r.queries.DeleteTrip(ctx, validatedId); err != nil {
+		return err
+	}
+
+	return nil
+}
