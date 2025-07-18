cb5eed72753fb116a65f545e1d1e146678743704
hata0 <haataa0100@gmail.com>
2025-07-18 09:56:22 +0900

    feat: ユーザーテーブルの作成と削除用のマイグレーションファイルを追加

---
+++ internal/infrastructure/postgres/sql/migrations/000002_create_users_table.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS users (
+  id UUID PRIMARY KEY,
+  email TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL,
+  updated_at TIMESTAMPTZ NOT NULL
+);
