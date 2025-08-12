2dcdf73daa8b94ef2214f8b238ecf7b8266425d7
hata0 <haataa0100@gmail.com>
2025-08-12 23:17:09 +0900

    feat: IDGeneratorとTimeProviderのモックを生成するためのコードを追加

--- internal/usecase/services/id.go
+++ internal/usecase/services/id.go
@@ -1,5 +1,6 @@
 package services
 
+//go:generate mockgen -destination mock/id.go github.com/hata0/travel-api/internal/usecase/services IDGenerator
 type IDGenerator interface {
 	Generate() string
 }
--- internal/usecase/services/time.go
+++ internal/usecase/services/time.go
@@ -2,6 +2,7 @@ package services
 
 import "time"
 
+//go:generate mockgen -destination mock/time.go github.com/hata0/travel-api/internal/usecase/services TimeProvider
 type TimeProvider interface {
 	Now() time.Time
 }
