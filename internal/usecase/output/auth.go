71d8f0b55292cecb89fd31a81f9434e329aa4c49
hata0 <haataa0100@gmail.com>
2025-08-13 10:09:42 +0900

    feat: outoutを改善

--- internal/usecase/output/auth.go
+++ internal/usecase/output/auth.go
@@ -1,10 +1,25 @@
 package output
 
+import "github.com/hata0/travel-api/internal/domain"
+
 type RegisterOutput struct {
 	UserID string
 }
 
+func NewRegisterOutput(userID domain.UserID) *RegisterOutput {
+	return &RegisterOutput{
+		UserID: userID.String(),
+	}
+}
+
 type TokenPairOutput struct {
-	Token        string
+	AccessToken  string
 	RefreshToken string
+}
+
+func NewTokenPairOutput(accessToken, refreshToken string) *TokenPairOutput {
+	return &TokenPairOutput{
+		AccessToken:  accessToken,
+		RefreshToken: refreshToken,
+	}
 }
