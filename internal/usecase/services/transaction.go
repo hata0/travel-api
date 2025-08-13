4edb11984a33bea65ed257af25190681f2973049
hata0 <haataa0100@gmail.com>
2025-08-13 10:04:17 +0900

    feat: TransactionManagerインターフェースを新規作成し、データベーストランザクションの実行機能を追加

---
+++ internal/usecase/services/transaction.go
@@ -0,0 +1,11 @@
+package services
+
+import "context"
+
+//go:generate mockgen -destination mock/transaction.go github.com/hata0/travel-api/internal/usecase/services TransactionManager
+type TransactionManager interface {
+	// RunInTx は指定された関数をデータベーストランザクション内で実行します。
+	// 関数がエラーを返した場合、トランザクションはロールバックされます。
+	// それ以外の場合、トランザクションはコミットされます。
+	RunInTx(ctx context.Context, fn func(ctx context.Context) error) error
+}
